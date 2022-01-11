a2253e6b90d0c5030dc180b9adc77764e30cbc03
Alfrugo <adiez@mtm-inc.net>
2022-01-10 19:50:46 -0600

    added all the inputs for the README file

---
+++ index.js
@@ -0,0 +1,162 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+
+const questions = () => {
+    return inquirer.prompt([
+        {
+            name: 'title',
+            type: 'input',
+            message: 'Please enter your project title: ', 
+            validate: titleInput => {
+                if (titleInput) {
+                  return true;
+                } else {
+                  console.log('Please enter title!');
+                  return false;
+                }
+              }
+        },
+        {
+            name: 'description',
+            type: 'input',
+            message: 'Please enter description for your project: ',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                  return true;
+                } else {
+                  console.log('Please enter a description!');
+                  return false;
+                }
+              }
+        },
+        {
+            name: 'installation',
+            type: 'input',
+            message: 'Include instructions for installation: ',
+            validate: installationInput => {
+                if (installationInput) {
+                  return true;
+                } else {
+                  console.log('Please enter the freaking instructions!');
+                  return false;
+                }
+              }
+        },
+        {
+            name: 'usage',
+            type: 'input',
+            message: 'Please add usage information: ',
+            validate: usageInput => {
+                if (usageInput) {
+                  return true;
+                } else {
+                  console.log('Sorry, but we need usage info!');
+                  return false;
+                }
+              }
+        },
+        {
+            name: 'contribution',
+            type: 'input',
+            message: 'Please enter the contribution guidelines: ',
+            validate: contributionInput => {
+                if (contributionInput) {
+                  return true;
+                } else {
+                  console.log('Please enter the contribution guidelines!');
+                  return false;
+                }
+              }
+        },
+        {
+            name: 'test',
+            type: 'input',
+            message: 'Please enter testing instruction: ',
+            validate: testInput => {
+                if (testInput) {
+                  return true;
+                } else {
+                  console.log('Please enter the testing instruction!!');
+                  return false;
+                }
+              }
+        },
+
+        {
+            name: 'licensing',
+            type: 'list',
+            message: 'What type of license does your project have: ',
+            choices: ['Community', 'MIT', 'GNU GPLv3']
+        },
+
+        {
+            name: 'githubUser',
+            type: 'input',
+            message: 'What is your Github username: ',
+            validate: userInput => {
+                if (userInput) {
+                  return true;
+                } else {
+                  console.log('Please enter your Github user!!');
+                  return false;
+                }
+              }
+        },
+        {
+            name: 'githubUser',
+            type: 'input',
+            message: 'What is your Github username: ',
+            validate: userInput => {
+                if (userInput) {
+                  return true;
+                } else {
+                  console.log('Please enter your Github user!!');
+                  return false;
+                }
+              }
+        },
+        {
+            name: 'email',
+            type: 'input',
+            message: 'Enter your email address: ',
+            validate: emailInput => {
+                if (emailInput) {
+                  return true;
+                } else {
+                  console.log('Please enter your email!!');
+                  return false;
+                }
+              }
+        },
+
+
+
+
+
+    ]).then(results => {
+        console.log(results);
+        const fileName = `./${results.title.split(' ').join('').toLowerCase()}.md`
+        fs.writeFile(fileName, JSON.stringify(results), {}, error => {
+            if (error) {
+                console.error(error);
+            } else { 
+                console.log('Success!')
+            }
+        })
+    });
+}
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+questions();
+
